b7b20a130cf7dde17249edbf70dbb020c08f0b6f
Absolute412 <kingalvin412@example.com>
2025-06-23 00:51:50 +0100

    Fix responsive navbar and dark mode planner text/icon color

--- meal-planner-frontend/src/components/Navbar.jsx
+++ meal-planner-frontend/src/components/Navbar.jsx
@@ -1,29 +1,50 @@
 import {Link} from "react-router-dom"
 import MealIcon from "../assets/icons/meal.svg"
+import { FaTimes, FaBars, FaMoon} from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FiSun } from "react-icons/fi";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem("theme") || "light";
+    });
+
+    useEffect(() => {
+        document.body.className = theme;
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(prev => (prev === "light" ? "dark" : "light"));
+    }
+
+    const toggleMenu = () => setMenuOpen(!menuOpen);
     return (
         <div>
             <nav className="nav-container">
                 <Link to="/" style={{ display: "flex", alignItems: "center", gap: "0.5rem", textDecoration: "none" }}>
                     <img src={MealIcon} alt="Meal Icon" style={{ width: "24px", height: "24px" }}/>
-                    <span 
-                        style={{
-                            color: "#333", 
-                            fontSize: "1.5rem", 
-                            fontWeight: "bold",
-                            textDecoration: "underline",
-                            textDecorationColor: "#f0c040",
-                            textUnderlineOffset: "4px"
-                            }}>
-                                Planner.
+                    <span className="planner-text">
+                        Planner.
                     </span>
                 </Link>
 
-                <div className="navlinks">
-                    <Link to="/">Home</Link>
-                    <Link to="/addmeal">Add Meal</Link>
-                    <Link to="/about">About</Link>
+                <div className="nav-right">
+                    <button onClick={toggleTheme} className="theme-toggle">
+                        {theme === "light" ? <FaMoon /> : <FiSun style={{color: "#f0f0f0"}}/>}
+                    </button>
+
+                    <div className={`navlinks ${menuOpen ? "open" : ""}`}>
+                        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+                        <Link to="/addmeal" onClick={() => setMenuOpen(false)}>Add Meal</Link>
+                        <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+                    </div>
+
+                    <div className="burger-icon" onClick={toggleMenu}>
+                        {menuOpen ? <FaTimes size={22} /> : <FaBars  size={22}/>}
+                    </div>
                 </div>
             </nav>
         </div>
